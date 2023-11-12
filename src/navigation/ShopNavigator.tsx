19b1ced11d29d01ef3baa7a06d6f675fc9235cd1
Ahmad IBRAHIM <ahmadji.ibr11@gmail.com>
2023-11-12 23:46:36 +0100

    create products navigation

---
+++ src/navigation/ShopNavigator.tsx
@@ -0,0 +1,31 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import ProductOverviewScreen from "../screens/shop/ProductOverviewScreen";
+import {Colors} from "react-native/Libraries/NewAppScreen";
+import {Platform} from "react-native";
+import React from "react";
+
+const Stack = createStackNavigator();
+
+const ProductsNavigator = (): React.JSX.Element => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: Platform.OS === 'android' ? Colors.black : 'black',
+                },
+                headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                },
+            }}
+        >
+            <Stack.Screen
+                name="ProductsOverview"
+                component={ProductOverviewScreen}
+                options={{title: 'Products Overview'}}
+            />
+        </Stack.Navigator>
+    );
+};
+
+export default ProductsNavigator;
