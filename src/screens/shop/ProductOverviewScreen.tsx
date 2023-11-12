6bfb875e47b5b7e0f2e43b7097e6cdbae3cb3ce8
Ahmad IBRAHIM <ahmadji.ibr11@gmail.com>
2023-11-12 23:47:07 +0100

    create products overview screen (basic without style..)

---
+++ src/screens/shop/ProductOverviewScreen.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {FlatList, Text} from 'react-native';
+import {useSelector} from "react-redux";
+import {RootState} from "../../../App";
+import Product from "../../ models/product";
+
+type Props = {
+    navigation?: any;
+};
+const ProductOverviewScreen = (props: Props) => {
+    const products: Product[] = useSelector((state: RootState) => state.products.availableProducts);
+    return (
+        <FlatList
+            data={products}
+            keyExtractor={(item: Product) => item.id}
+            renderItem={
+                (itemData: { item: Product }) => {
+                    return (
+                        <Text>{itemData.item.title}</Text>
+                    )
+                }
+            }/>
+    );
+}
+
+ProductOverviewScreen.navigationOptions = {
+    headerTitle: 'All Products',
+}
+
+export default ProductOverviewScreen;
