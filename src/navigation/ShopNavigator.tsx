2254a731b496e2ac654adcdd69fe421c33b60594
Ahmad IBRAHIM <ahmadji.ibr11@gmail.com>
2023-11-15 13:31:23 +0100

    create the burger menu

--- src/navigation/ShopNavigator.tsx
+++ src/navigation/ShopNavigator.tsx
@@ -7,24 +7,30 @@ import React from "react";
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import HeaderButton from "../components/UI/HeaderButton";
 import CartScreen from "../screens/shop/CartScreen";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+import OrdersScreen from "../screens/shop/OrdersScreen";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const defaultNavOptions = {
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+    headerTitleStyle: {
+        fontFamily: 'OpenSans-Bold',
+    },
+    headerBackTitleStyle: {
+        fontFamily: 'OpenSans-Regular',
+    },
+};
 
 const ProductsNavigator = (): React.JSX.Element => {
     return (
         <Stack.Navigator
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
-                },
-                headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
-                headerTitleStyle: {
-                    fontFamily: 'OpenSans-Bold',
-                },
-                headerBackTitleStyle: {
-                    fontFamily: 'OpenSans-Regular',
-                },
-            }}
+            screenOptions={defaultNavOptions}
         >
             <Stack.Screen
                 name="ProductsOverview"
@@ -38,6 +44,15 @@ const ProductsNavigator = (): React.JSX.Element => {
                                     title="Cart"
                                     iconName="shopping-cart"
                                     onPress={() => navData.navigation.navigate('Cart')}
+                                />
+                            </HeaderButtons>
+                        ),
+                        headerLeft: () => (
+                            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                                <Item
+                                    title="Menu"
+                                    iconName="bars"
+                                    onPress={() => navData.navigation.toggleDrawer()}
                                 />
                             </HeaderButtons>
                         ),
@@ -60,8 +75,64 @@ const ProductsNavigator = (): React.JSX.Element => {
                     title: 'Cart',
                 }}
             />
+
         </Stack.Navigator>
     );
 };
 
-export default ProductsNavigator;
+const OrdersNavigator = (): React.JSX.Element => {
+    return (
+        <Stack.Navigator
+            screenOptions={defaultNavOptions}
+        >
+            <Stack.Screen
+                name="OrdersScreen"
+                component={OrdersScreen}
+                options={
+                    (navData: any) => ({
+                        title: 'Your Orders',
+                        headerLeft: () => (
+                            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                                <Item
+                                    title="Menu"
+                                    iconName="bars"
+                                    onPress={() => navData.navigation.toggleDrawer()}
+                                />
+                            </HeaderButtons>
+                        ),
+                    })
+                }
+            />
+        </Stack.Navigator>
+    );
+}
+
+const ShopNavigator = (): React.JSX.Element => {
+    return (
+        <Drawer.Navigator
+        >
+            <Drawer.Screen
+                name="Products"
+                component={ProductsNavigator}
+                options={{
+                    headerShown: false,
+                    drawerIcon: () => (
+                        <Icon name="shopping-cart" size={20} color="black"/>
+                    ),
+                }}
+            />
+            <Drawer.Screen
+                name="Orders"
+                component={OrdersNavigator}
+                options={{
+                    headerShown: false,
+                    drawerIcon: () => (
+                        <Icon name="list" size={20} color="black"/>
+                    ),
+                }}
+            />
+        </Drawer.Navigator>
+    );
+}
+
+export default ShopNavigator;
